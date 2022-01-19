d1d58fdeeda71d0490e4c3d003ddce2833b3ca77
BertaF <74141481+BertaF@users.noreply.github.com>
2022-01-19 02:02:01 +0000

    Added GameObjectPoolManager and GameObjectSpawner scripts
    1. Created a game object pool manager file. Currently has 1 pool with 20 enemy plants.
    2. Created a basic game object spawner. It allows a one-time spawn or a spawn routine. It is currently spawning two enemy plants as a test.
    3. Added the assets for an enemy plant, with animations disabled for now.

---
+++ Assets/Scripts/GameObjectPoolManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectPoolManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    #region Singleton
+    public static GameObjectPoolManager Instance;
+    private void Awake()
+    {
+        Instance = this;
+    }
+    #endregion
+
+    #region Variables
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+    #endregion
+
+    private void Start()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+        // Creates a queue of instantiated objects for each pool
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.size; ++i)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                obj.transform.SetParent(transform); // Keep the inactive objects inside the pool manager hierarchy
+                objectPool.Enqueue(obj);
+            }
+
+            // Fills the pool dict with each queue
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+    }
+
+    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+    {
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            Debug.LogWarning("Pool with tag: " + tag + " , does not exist!");
+            return  null;
+        }
+
+        // Initialises the game object stored in this queue
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+
+        // Add this object back to the queue so it can be reused
+        poolDictionary[tag].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
+    }
+}
---
+++ Assets/Scripts/GameObjectSpawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectSpawner : MonoBehaviour
+{
+    [Header("Object To Spawn Settings")]
+    [SerializeField] private GameObject objectToSpawn;
+    [SerializeField] private GameObject parent;
+
+    [Header("Number Of Objects To Spawn")]
+    [SerializeField] private int numberToSpawnAtPos = 0;
+    [SerializeField] private int maxObjectsAtPos = 0;
+
+    [Header("Timer Settings")]
+    [SerializeField] private float spawnRate = 0.0f;
+
+    #region Variables
+    private bool stopSpawnRoutine = false;
+    private GameObjectPoolManager objectPoolMgr;
+    #endregion
+
+    private void Start()
+    {
+        objectPoolMgr = GameObjectPoolManager.Instance;
+        StartCoroutine(SpawnRoutine());
+    }
+
+    private IEnumerator SpawnRoutine()
+    {
+        while (!stopSpawnRoutine)
+        {
+            for (int i = 0; i < numberToSpawnAtPos; ++i)
+            {
+                if (parent.transform.GetChild(i).childCount >= maxObjectsAtPos)
+                    continue;
+
+                GameObject instObject = objectPoolMgr.SpawnFromPool(objectToSpawn.tag, parent.transform.GetChild(i).position, parent.transform.GetChild(i).transform.rotation);
+                instObject.transform.SetParent(parent.transform.GetChild(i).transform);
+            }
+
+            // Allows the routine to run only once
+            if (spawnRate == 0)
+            {
+                stopSpawnRoutine = true;
+            }
+            yield return new WaitForSeconds(spawnRate);
+        }
+    }
+}
