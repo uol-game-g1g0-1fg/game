e7f84b32817807a645a2f556e331b0599c580f81
TomaszStraszewski <tomasz@justwontdie.com>
2022-02-06 11:12:55 +0000

    Adds Event 'Road Blocked' -> pullable rock

---
+++ Assets/Scripts/PullableItem.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullableItem : MonoBehaviour
+{
+    public Rigidbody[] bodies;
+    public GameObject rockToPull;
+    private bool isPulling = false;
+    private Collider arm;
+    private Vector3 armOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bodies = GetComponentsInChildren<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( arm && isPulling)
+        rockToPull.transform.position = arm.transform.position + armOffset;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "MechArm")
+        {
+            Debug.Log("Pulling!...");
+
+            isPulling = true;
+            arm = other;
+            armOffset = rockToPull.transform.position - arm.transform.position;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "MechArm")
+        {
+            Debug.Log("Pulling!...");
+
+            isPulling = false;
+
+            foreach (Rigidbody body in bodies)
+            {
+                body.isKinematic = false;
+            }
+        }
+    }
+}
