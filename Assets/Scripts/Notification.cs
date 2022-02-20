602c68e13fa5c74ea0780cdbbd670cd6cbc57a9a
Matej Kramny <matej@matej.me>
2022-02-20 11:29:52 +0000

    notification system

---
+++ Assets/Scripts/Notification.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum NotificationType
+{
+  Trigger, Time
+}
+
+public class Notification : MonoBehaviour
+{
+  public NotificationType type;
+  public string text;
+  [Tooltip("For Time type")]
+  public float timeDelaySeconds;
+
+  public float hideAfterSeconds = 10;
+
+  private NotificationManager notificationManager;
+
+  void Start()
+  {
+    print("Registered notification");
+    notificationManager = GameObject.Find("/UI Manager").GetComponent<NotificationManager>();
+
+    if (type == NotificationType.Time)
+    {
+      print("time based");
+      StartCoroutine(ActivateAfterDelay());
+    }
+  }
+
+  IEnumerator ActivateAfterDelay()
+  {
+    yield return new WaitForSeconds(timeDelaySeconds);
+
+    notificationManager.activate(text, hideAfterSeconds);
+  }
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.tag != "Player") return;
+    print("triggered" + other.tag);
+
+    notificationManager.activate(text, hideAfterSeconds);
+  }
+}
---
+++ Assets/Scripts/NotificationManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class NotificationManager : MonoBehaviour
+{
+  [Tooltip("Notification Prefab")]
+  public GameObject notificationBox;
+  public Text text;
+
+  private float hideAtTime = 0;
+  float hideDurationSeconds = 1f;
+
+  void Start()
+  {
+    var initial = "Your \"Explorer\" Submarine is damaged! Main engines are non-functional. Find the \"Fusion Core\" to fix it! To move use WSAD. Once it is done, you will be able to progress to the surface. Be aware of hazards! Good Luck!";
+
+    activate(initial, 10);
+  }
+
+  void Update()
+  {
+    if (hideAtTime > 0 && Time.unscaledTime > hideAtTime)
+    {
+      StartCoroutine(hideNotification());
+      hideAtTime = 0;
+    }
+  }
+
+  IEnumerator hideNotification()
+  {
+    float t = 0f;
+
+    while (t < hideDurationSeconds)
+    {
+      t += Time.unscaledDeltaTime;
+      setOpacity(Mathf.Lerp(1f, 0f, t / hideDurationSeconds));
+      yield return null;
+    }
+  }
+
+  void setOpacity(float opacity)
+  {
+    notificationBox.GetComponent<CanvasGroup>().alpha = opacity;
+    // var renderer = notificationBox.transform.Find("Speaker").GetComponent<RawImage>();
+    // var c = renderer.material.color;
+    // c.a = opacity;
+    // renderer.material.color = c;
+  }
+
+  public void activate(string value, float hideAfterSeconds)
+  {
+    setOpacity(1);
+    text.text = value;
+    hideAtTime = Time.unscaledTime + hideAfterSeconds;
+  }
+}
