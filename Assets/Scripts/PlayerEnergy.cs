3d3e71f0c711f0037bb83ba35f81e921456cdd0c
adammyhre <38876398+adammyhre@users.noreply.github.com>
2022-02-26 14:18:28 -0800

    Energy System (#49)
    Adds an energy system.  Energy affects the player's ability to move
    by impacting their moveSpeed proportional to the amount of energy
    they have left.  Energy is consumed over time.  Energy can be
    replenished using pickups.

---
+++ Assets/Scripts/PlayerEnergy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEnergy : MonoBehaviour {
+
+    [SerializeField] float energyReserve = 100.0f;
+    [SerializeField] AnimationCurve energyRate;
+    [SerializeField, Range(0.1f, 1f)] float loss = 0.1f;
+    readonly float maxEnergy = 100.0f;
+    readonly float minEnergy = 0.0f;
+
+    public float Power => energyRate.Evaluate(energyReserve);
+    public float Velocity => energyReserve / maxEnergy;
+
+    void Update() {
+        energyReserve -= loss * Time.deltaTime;
+        if (energyReserve <= minEnergy) energyReserve = minEnergy;
+    }
+
+    public void Increase(float amount) {
+        energyReserve += amount;
+        if (energyReserve > maxEnergy) energyReserve = maxEnergy;
+    }
+}
