6f1d87261040f0018e9c9864dc05b96999445831
BertaF <74141481+BertaF@users.noreply.github.com>
2022-02-28 01:17:24 +0000

    UI: Added a pause menu
    1. Updated MainMenu.cs with logic that listens for the P key and enables/disables the game pause menu. This menu allows the player to access the main menu too.
    
    2. Added a simple overlay window transition animation, to make the transition from in-game to pause menu smoother.
    
    3. Fixed code indentation in NotificationManager.cs, and added a check to prevent the notifications from being cleared when the pause menu is enabled. The NotificationManager.cs now inherits from Main Menu class. (TODO: Rename this class, does more than manage Main Menu logic)

--- Assets/Scripts/NotificationManager.cs
+++ Assets/Scripts/NotificationManager.cs
@@ -4,80 +4,80 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class NotificationManager : MonoBehaviour
+public class NotificationManager : MainMenu
 {
-  [Tooltip("Notification Prefab")]
-  public GameObject notificationBox;
-  public Text text;
+    [Tooltip("Notification Prefab")]
+    public GameObject notificationBox;
+    public Text text;
 
-  private float hideAtTime = 0;
-  float hideDurationSeconds = 1f;
+    private float hideAtTime = 0;
+    float hideDurationSeconds = 1f;
 
-  private bool hasShownPlantMessage = false;
-  private bool hasShownPickupCoreMessage = false;
-  private bool hasShownTreasureMessage = false;
+    private bool hasShownPlantMessage = false;
+    private bool hasShownPickupCoreMessage = false;
+    private bool hasShownTreasureMessage = false;
 
-  void Start()
-  {
-    var initial = "Your Submarine is damaged and cannot move. Find the Fusion Core to fix it. Use WSAD keys to move, G to pick up items and F to fire your harpoon";
-
-    activate(initial, 10);
-  }
-
-  void Update()
-  {
-    if (hideAtTime > 0 && Time.unscaledTime > hideAtTime)
+    void Start()
     {
-      StartCoroutine(hideNotification());
-      hideAtTime = 0;
+        var initial = "Your Submarine is damaged and cannot move. Find the Fusion Core to fix it. Use WSAD keys to move, G to pick up items and F to fire your harpoon";
+
+        activate(initial, 10);
     }
-  }
 
-  IEnumerator hideNotification()
-  {
-    float t = 0f;
-
-    while (t < hideDurationSeconds)
+    void Update()
     {
-      t += Time.unscaledDeltaTime;
-      setOpacity(Mathf.Lerp(1f, 0f, t / hideDurationSeconds));
-      yield return null;
+        if (hideAtTime > 0 && Time.unscaledTime > hideAtTime && !MainMenu.m_IsGamePaused)
+        {
+            StartCoroutine(hideNotification());
+            hideAtTime = 0;
+        }
     }
-  }
 
-  void setOpacity(float opacity)
-  {
-    notificationBox.GetComponent<CanvasGroup>().alpha = opacity;
-  }
+    IEnumerator hideNotification()
+    {
+        float t = 0f;
 
-  public void activate(string value, float hideAfterSeconds)
-  {
-    setOpacity(1);
-    text.text = value;
-    hideAtTime = Time.unscaledTime + hideAfterSeconds;
-  }
+        while (t < hideDurationSeconds)
+        {
+            t += Time.unscaledDeltaTime;
+            setOpacity(Mathf.Lerp(1f, 0f, t / hideDurationSeconds));
+            yield return null;
+        }
+    }
 
-  public void OnPlantProjectileFired()
-  {
-    if (hasShownPlantMessage) return;
-    hasShownPlantMessage = true;
+    void setOpacity(float opacity)
+    {
+        notificationBox.GetComponent<CanvasGroup>().alpha = opacity;
+    }
 
-    activate("Watch out! Some entities are unfriendly and can cause damage to your vessel", 10);
-  }
+    public void activate(string value, float hideAfterSeconds)
+    {
+        setOpacity(1);
+        text.text = value;
+        hideAtTime = Time.unscaledTime + hideAfterSeconds;
+    }
 
-  public void OnEnableCore()
-  {
-    if (hasShownPickupCoreMessage) return;
-    hasShownPickupCoreMessage = true;
+    public void OnPlantProjectileFired()
+    {
+        if (hasShownPlantMessage) return;
+        hasShownPlantMessage = true;
 
-    activate("Your Submarine now has a Fusion Core installed and is able to ascend to the surface!", 10);
-  }
+        activate("Watch out! Some entities are unfriendly and can cause damage to your vessel", 10);
+    }
 
-  public void OnPickupTreasure()
-  {
-    if (hasShownTreasureMessage) return;
-    hasShownTreasureMessage = true;
+    public void OnEnableCore()
+    {
+        if (hasShownPickupCoreMessage) return;
+        hasShownPickupCoreMessage = true;
 
-    activate("Nice find! Looks like you found some Treasure. Pick these up to increase your Game score", 10);
-  }
+        activate("Your Submarine now has a Fusion Core installed and is able to ascend to the surface!", 10);
+    }
+
+    public void OnPickupTreasure()
+    {
+        if (hasShownTreasureMessage) return;
+        hasShownTreasureMessage = true;
+
+        activate("Nice find! Looks like you found some Treasure. Pick these up to increase your Game score", 10);
+    }
 }
--- Assets/UI/main menu/MainMenu.cs
+++ Assets/UI/main menu/MainMenu.cs
@@ -6,10 +6,44 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     #region Property Inspector Variables
+    [SerializeField] GameObject m_PauseMenu;
     [SerializeField] GameObject m_ShowOnDeath;
     [SerializeField] GameObject m_ShowOnWin;
     [SerializeField] GameObject[] m_HideOnFinish;
     #endregion
+
+    #region Variables
+    public static bool m_IsGamePaused = false;
+    #endregion
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (m_IsGamePaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        m_PauseMenu.SetActive(false);
+        m_IsGamePaused = false;
+        Time.timeScale = 1.0f;
+    }
+
+    public void PauseGame()
+    {
+        m_PauseMenu.SetActive(true);
+        m_IsGamePaused = true;
+        Time.timeScale = 0.0f;
+    }
 
     public void StartGame()
     {
