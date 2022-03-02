1d73a415d3118a76676ca4c4212b3d108d10198f
= <chris.a.hain@gmail.com>
2022-03-01 20:22:05 -0500

    gauge now properly shows depth

--- Assets/UI/in game/needleUpdate.cs
+++ Assets/UI/in game/needleUpdate.cs
@@ -11,9 +11,18 @@ public class needleUpdate : MonoBehaviour
     [SerializeField]
     private Slider slider;
 
+    [SerializeField]
+    private GameObject player;
+
+    [SerializeField]
+    private float levelHeight = 136f;
+
     private float currentDepth = 0;
     private float targetDepth = 0;
     private float needleSpeed = 100.0f;
+
+    private float levelRange = 136f - 0.75f;
+    private float gaugeRange = 130 - (-130);
 
 
     public void setDepthFromSlider()
@@ -23,20 +32,23 @@ public class needleUpdate : MonoBehaviour
 
     public void setDepth(float amt)
     {
-        targetDepth = amt;
+        //targetDepth = amt - levelHeight;
+
+        targetDepth = (((amt - 0.75f) * gaugeRange) / levelRange) - 130f;
     }
 
     void updateDepth()
     {
-        if(targetDepth > currentDepth)
+        if (targetDepth > currentDepth)
         {
             currentDepth += Time.deltaTime * needleSpeed;
-            currentDepth = Mathf.Clamp(currentDepth, 0.0f, targetDepth);
+            currentDepth = Mathf.Clamp(currentDepth, -130f, targetDepth);
 
-        } else if(targetDepth < currentDepth)
+        }
+        else if (targetDepth < currentDepth)
         {
             currentDepth -= Time.deltaTime * needleSpeed;
-            currentDepth = Mathf.Clamp(currentDepth, targetDepth, 260.0f);
+            currentDepth = Mathf.Clamp(currentDepth, targetDepth, 130.0f);
         }
 
         setNeedle();
@@ -45,11 +57,16 @@ public class needleUpdate : MonoBehaviour
     void setNeedle()
     {
         needleImg.transform.localEulerAngles = new Vector3(0, 0, currentDepth);
+        //needleImg.transform.rotation = Quaternion.Euler(0, 0, currentDepth);
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        setDepth(player.transform.position.y);
+        Debug.Log("depth: " + targetDepth);
+
         if (targetDepth != currentDepth)
         {
             updateDepth();
