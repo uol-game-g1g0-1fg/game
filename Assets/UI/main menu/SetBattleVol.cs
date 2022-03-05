13ea1cf702fce0852c9c30d603a7132a599b3e98
= <chris.a.hain@gmail.com>
2022-03-05 08:16:45 -0500

    added volume menu functionality

---
+++ Assets/UI/main menu/SetBattleVol.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SetBattleVol : MonoBehaviour
+{
+
+	public AudioMixer mixer;
+
+	public void SetLevel(float sliderVal)
+	{
+		mixer.SetFloat("BattleVol", Mathf.Log10(sliderVal) * 20);
+	}
+
+}
