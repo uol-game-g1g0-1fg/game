8e84442bc37b3acc24bf9d22eaf9e4c948111524
= <chris.a.hain@gmail.com>
2022-03-12 07:35:07 -0500

    added player score scripts and associated score message on game won screen

---
+++ Assets/Scripts/ScoreTextUpdate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreTextUpdate : MonoBehaviour
+{
+
+    private TextMeshProUGUI finalScoreText;
+    
+    [SerializeField]
+    private int score = 0;
+
+    void Start()
+    {
+        finalScoreText = GetComponent<TextMeshProUGUI>();
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score = GameObject.Find("Player").GetComponent<PlayerScore>().playerScore;
+
+        if(score == 0){
+            finalScoreText.text = "You didn't find any treasures on your journey to the surface.";
+        }else{
+            finalScoreText.text = "You found a total of " + score.ToString() + " treasures on your journey to the surface!";
+        }
+
+    }
+}
